6cd1429cd0cbbb82e5f20ce3810c97e6a64bd8fb
shenjiahao <1843781563@qq.com>
2020-08-19 02:09:13 +0800

    Remove static resources folder

--- ZJ-Proxy/config/config.js
+++ ZJ-Proxy/config/config.js
@@ -1,5 +1,5 @@
 // exports.ip = '127.0.0.1';
 // exports.port = '3000';
 // exports.serverIP = '127.0.0.1';
-exports.serverIP = '127.0.0.1:5000'
+exports.serverIP = '39.100.234.92:5000';
 exports.interval = 3000;
